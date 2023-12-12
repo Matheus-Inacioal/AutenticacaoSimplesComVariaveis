dfd4652beabff8f258b6cc501a462d6dc0ce6b66
Matheus <malmeidaarruda2@gmail.com>
2023-12-12 09:47:04 -0300

    0.3.0

--- app/src/main/java/com/example/myapplication/CadastroActivity.kt
+++ app/src/main/java/com/example/myapplication/CadastroActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class CadastroActivity : AppCompatActivity() {
 
@@ -15,28 +17,58 @@ class CadastroActivity : AppCompatActivity() {
     private lateinit var etSenhaCadastro: EditText
     private lateinit var btnCadastrar: Button
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
 
+        // Inicialização das views
         etNomeUsuario = findViewById(R.id.etNomeUsuario)
         etEmail = findViewById(R.id.etEmail)
         etSenhaCadastro = findViewById(R.id.etSenhaCadastro)
         btnCadastrar = findViewById(R.id.btnCadastrar)
 
+        // Inicialização do FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Configurar um listener de clique para o botão de cadastro
         btnCadastrar.setOnClickListener {
             val nomeUsuario = etNomeUsuario.text.toString()
             val email = etEmail.text.toString()
             val senha = etSenhaCadastro.text.toString()
 
-            // Armazenar as credenciais no backend (variáveis estáticas para este exemplo).
-            CredenciaisArmazenadas.nomeUsuario = nomeUsuario
-            CredenciaisArmazenadas.email = email
-            CredenciaisArmazenadas.senha = senha
+            // Criar um usuário no Firebase
+            firebaseAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Se o cadastro for bem-sucedido, obter o usuário atual
+                        val user: FirebaseUser? = firebaseAuth.currentUser
 
-            // Ir para a tela de login
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+                        // Atualizar o perfil do usuário com o nome
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setDisplayName(nomeUsuario)
+                            .build()
+
+                        user?.updateProfile(profileUpdates)
+
+                        // Ir para a tela de login após o cadastro
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Se falhar, tratar o erro
+                        task.exception?.let { exception ->
+                            // Tratar a exceção aqui
+                            // Exibir uma mensagem de erro para o usuário
+                            Toast.makeText(
+                                this@CadastroActivity,
+                                "Erro ao cadastrar: ${exception.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
         }
     }
 
@@ -45,4 +77,5 @@ class CadastroActivity : AppCompatActivity() {
         var email: String = ""
         var senha: String = ""
     }
+
 }
--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,27 +17,50 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var tvErro: TextView
     private lateinit var tvCadastro: TextView
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicialização das views
         etUsuario = findViewById(R.id.etUsuario)
         etSenha = findViewById(R.id.etSenha)
         btnLogin = findViewById(R.id.btnLogin)
         tvErro = findViewById(R.id.tvErro)
         tvCadastro = findViewById(R.id.tvCadastro)
 
+        // Inicialização do FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Configurar um listener de clique para o botão de login
         btnLogin.setOnClickListener {
             val nomeUsuario = etUsuario.text.toString()
             val senha = etSenha.text.toString()
 
-            if (validarCredenciais(nomeUsuario, senha)) {
-                val intent = Intent(this, WelcomeActivity::class.java)
-                intent.putExtra("nomeUsuario", nomeUsuario)
-                startActivity(intent)
-            } else {
-                tvErro.text = "Credenciais inválidas"
-            }
+            // Autenticar usando o Firebase
+            firebaseAuth.signInWithEmailAndPassword(CadastroActivity.CredenciaisArmazenadas.email, senha)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Se a autenticação for bem-sucedida, ir para a tela de boas-vindas
+                        val intent = Intent(this, WelcomeActivity::class.java)
+                        intent.putExtra("nomeUsuario", nomeUsuario)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Se falhar, tratar o erro
+                        task.exception?.let { exception ->
+                            // Tratar a exceção aqui
+                            // Exemplo: exibir uma mensagem de erro para o usuário
+                            tvErro.text = "Credenciais inválidas"
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Erro ao fazer login: ${exception.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
         }
 
         // Configurar o OnClickListener para o TextView de cadastro
@@ -43,11 +68,6 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun validarCredenciais(nomeUsuario: String, senha: String): Boolean {
-        // Verificar as credenciais cadastradas
-        return nomeUsuario == CadastroActivity.CredenciaisArmazenadas.nomeUsuario && senha == CadastroActivity.CredenciaisArmazenadas.senha
     }
 }
 
